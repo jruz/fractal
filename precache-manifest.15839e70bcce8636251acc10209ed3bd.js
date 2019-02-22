b0aa82276a089bd8522314a6e04528ea536d9091
Javier Ruz <javi@jruz.io>
2019-02-22 23:29:42 +0100

    Updates

---
+++ precache-manifest.15839e70bcce8636251acc10209ed3bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47aa20018e6e32bb94b5",
+    "url": "/fractal/static/css/main.bdbd27ad.chunk.css"
+  },
+  {
+    "revision": "47aa20018e6e32bb94b5",
+    "url": "/fractal/static/js/main.47aa2001.chunk.js"
+  },
+  {
+    "revision": "5f56758b4254d9a87fa1",
+    "url": "/fractal/static/js/runtime~main.5f56758b.js"
+  },
+  {
+    "revision": "8b5ad378e91d9906a1fb",
+    "url": "/fractal/static/js/2.8b5ad378.chunk.js"
+  },
+  {
+    "revision": "c853396641a4b6e7f572d556370e2c74",
+    "url": "/fractal/index.html"
+  }
+];
